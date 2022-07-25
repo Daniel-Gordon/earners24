94d476214d765a119c774a180c75a45dd0cd366d
Daniel-Gordon <109922690+Daniel-Gordon@users.noreply.github.com>
2022-07-25 12:37:10 +0100

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="style1.css">
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  </head>
+  <body>
+    <div class="wrapper">
+      <input type="checkbox" id="btn" hidden>
+      <label for="btn" class="menu-btn">
+        <i class="fas fa-bars"></i>
+        <i class="fas fa-times"></i>
+      </label>
+      <nav id="sidebar">
+        <div class="title">Side Menu</div>
+        <ul class="list-items">
+          <li><a href="index.php#section-home" class="fas fa-home"></i>Home</a></li>
+          <li><a href="login-user.php" class="fas fa-stream"></i>Login</a></li>
+          <li><a href="signup-user.php" class="fas fa-cog"></i>SignUp/Register</a></li>
+          <li><a href="home.php" class="fas fa-address-book">My Account</a></li>
+          <li><a href="https://wa.link/1a5x8k" class="fas fa-sliders-h">Buy Coupon Code</a></li>
+          <li><a href="index.php#section-about" class="fas fa-user">About us</a></li>
+          <li><a href="mailto:info.earners24@gmail.com" class="fas fa-envelope">Contact us</a></li>
+          <div class="icons">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-github"></i></a>
+            <a href="#"><i class="fab fa-youtube"></i></a>
+          </div>
+        </ul>
+      </nav>
+    </div>
+    <div class="content">
+      <div class="header"></div>
+      <p></p>
+    </div>
+    </body>
+    </html>
